6b9523430d9a4e1681c0df52b00c15badcaf23f8
Bilasco Ioan Marius <marius.bilasco@univ-lille.fr>
2020-03-07 13:38:17 +0100

    TP3

--- metrics/pixels.js
+++ metrics/pixels.js
@@ -24,3 +24,33 @@ pixel_metrics.grid_rgb_edist=function(pixels_rgb_grid1, pixels_rgb_grid2) {
 
   return generic_metrics.euclidian_distance_btw_feature_vectors(pixels_rgb_grid1.cells,pixels_rgb_grid2.cells,dist_fun);
 }
+
+/*
+    pixel_metrics.gray_rgb_edist - computes euclidian distance between two rgb pixels considering them as gray pixels.
+*/
+pixel_metrics.gray_rgb_edist=function(pixel_rgb1, pixel_rgb2) {
+
+  return Math.abs(pixel_rgb1[0] - pixel_rgb2[0]+
+                  pixel_rgb1[1] - pixel_rgb2[1]+
+                  pixel_rgb1[2] - pixel_rgb2[2])/3;
+}
+
+/*
+    pixel_metrics.gray_rgb_edist - computes euclidian distance between two rgb pixels considering them as gray pixels.
+*/
+pixel_metrics.gray_rgb_edist=function(pixel_rgb1, pixel_rgb2) {
+
+  return Math.abs(pixel_rgb1[0] - pixel_rgb2[0]+
+                  pixel_rgb1[1] - pixel_rgb2[1]+
+                  pixel_rgb1[2] - pixel_rgb2[2])/3;
+}
+
+/*
+    pixel_metrics.visible_edist - computes similarity between two rgb pixels considering them as B&W pixels. if both B or both W than distance=0 otherwise 255
+*/
+pixel_metrics.visible_edist=function(pixel_rgb1, pixel_rgb2) {
+  var mean1=(pixel_rgb1[0]+pixel_rgb1[1]+pixel_rgb1[2]);
+  var mean2=(pixel_rgb2[0]+pixel_rgb2[1]+pixel_rgb2[2]);
+
+  return ((mean1==0 && mean2==0) || (mean1>0 && mean2>0))?0:255;
+}
